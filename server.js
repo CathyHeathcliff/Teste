b1678d555f8d88cd9b76b9656f510de443771766
CathyHeathcliff <isabela.molaz@etec.sp.gov.br>
2020-06-20 02:11:43 +0000

    Alterado

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/usuario', async function(request, response) {
+server.get('/', async function(request, response) {
     const dados = await database.select();
     return response.json(dados); 
 })
 
-server.post('/usuario', async function(request, response) {
+server.post('/', async function(request, response) {
     const {Nome, Telefone, Email, Senha}  = request.body;
     
     const result = await database.create(Nome, Telefone, Email, Senha);
@@ -23,7 +23,7 @@ server.post('/usuario', async function(request, response) {
     response.status(204).send();
 })
 
-server.put('/usuario/idUsuario/:idUsuario', async function(request, response) {
+server.put('/idUsuario/:idUsuario', async function(request, response) {
 
     const idUsuario = request.params.idUsuario;
     const {Nome, Telefone, Email, Senha} = request.body;
@@ -33,7 +33,7 @@ server.put('/usuario/idUsuario/:idUsuario', async function(request, response) {
     return response.status(204).send(); 
 })
 
-server.delete('/usuario/:idUsuario', async function(request, response) {
+server.delete('/:idUsuario', async function(request, response) {
 
     const idUsuario = request.params.idUsuario;
 
